ab9143c107156ccd245815685d428be45f3488fa
Nurzhas Nurbayev <montishok@gmail.com>
2023-07-15 18:27:15 +0600

    Seperating route logic

--- discordlikeapp/urls.py
+++ discordlikeapp/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-def home(request: any) -> HttpResponse:
-    return HttpResponse('Hello World !') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls'))
 ] 
