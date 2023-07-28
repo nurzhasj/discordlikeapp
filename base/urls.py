ba3afd03551d22aca7a3408ba14a47cae340a904
Nurzhas Nurbayev <montishok@gmail.com>
2023-07-29 05:27:31 +0600

    RoomForm generated

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<str:pk>/', views.room, name="room")
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 def home(request: any) -> HttpResponse:
@@ -13,3 +14,10 @@ def room(request: any, pk: str) -> HttpResponse:
     context = {'room' : room}  
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request: any) -> HttpResponse:
+    form = RoomForm()
+
+    context = {'form' : form} 
+
+    return render(request, 'base/room_form.html', context) 
