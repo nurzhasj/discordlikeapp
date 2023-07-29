8e7450eb5a29d171af99893379114a6f252c0f6f
Nurzhas Nurbayev <montishok@gmail.com>
2023-07-29 15:16:52 +0600

    CRUD operations done

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
 from .forms import RoomForm
@@ -18,6 +18,37 @@ def room(request: any, pk: str) -> HttpResponse:
 def createRoom(request: any) -> HttpResponse:
     form = RoomForm()
 
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form' : form} 
 
     return render(request, 'base/room_form.html', context) 
+
+def updateRoom(request: any, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+
+    form = RoomForm(instance=room)
+
+    context = {'form' : form}
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request: any, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj': room})
+    
