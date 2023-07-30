fa0024c116559fd9d777ed2ac926c3f6564dd325
Nurzhas Nurbayev <montishok@gmail.com>
2023-07-30 13:08:47 +0600

    Auth funcs were implemented

--- base/views.py
+++ base/views.py
@@ -2,7 +2,34 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .forms import RoomForm
+
+def loginPage(request: any) -> HttpResponse:
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            User.objects.get(username=username)
+        except:
+            messages.error(request, "User doesn't exists")
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password doesn't exists")
+
+    return render(request, 'base/login_register.html', {})
+
+def logoutUser(request: any) -> HttpResponse:
+    logout(request)
+    return redirect('home')
 
 # Create your views here.
 def home(request: any) -> HttpResponse:
